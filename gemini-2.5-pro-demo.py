90df4a81292bf9fe77ec8d1a54081456ec5e98e9
Murali312 <muralic312@gmail.com>
2025-12-02 20:35:46 +0530

    Connected to gemini models

---
+++ gemini-2.5-pro-demo.py
@@ -0,0 +1,19 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+print(os.getenv("GOOGLE_API_KEY"))
+
+# Configure with your API key
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Create a model instance
+model = genai.GenerativeModel("gemini-2.0-flash-lite")
+
+# Generate a simple response
+response = model.generate_content("What is the capital of India")
+
+# Print the output
+print(response.text)
--- openai_demo.py
+++ openai_demo.py
@@ -8,6 +8,8 @@ load_dotenv()
 
 client = OpenAI()  
 
+# client = OpenAI(api_key=os.getenv("TEST_API_KEY"))     -   If we change name of the key i.e., "OPENAI_API_KEY" with some other name (here - "TEST_API_KEY")
+
 
 response = client.responses.create(
     model="gpt-5-nano",
